e849dc129475fba6b811e412e05818d76c52337e
Rama <game_jecboom@mail.ru>
2022-12-12 19:05:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+var fs = require("fs");
+var request = require("request");
+var mime = require('mime-types');
+
+//Code by rama0dev
+
+exports.upload = function (authorization, projectId, file, url, callback) {
+    if(Math.floor(fs.statSync(file).size/1000)>261120){
+        callback({
+            "status": "error",
+            "message":"bigfile",
+            "filesize":Math.floor(fs.statSync(file).size/1000)
+        })
+    }else{
+        function getAuth(type, callback) {
+        request({
+            url: `https://api.glitch.com/v1/projects/${projectId}/policy?contentType=${type}`,
+            headers: {
+                'Content-Type': 'application/json',
+                "authorization": authorization
+            },
+            method: "GET"
+        }, (error, response, body) => {
+            body = JSON.parse(body);
+            callback(body)
+        })
+    }
+
+    var type = mime.lookup(file)
+
+    getAuth(type, (data) => {
+        if(data.code==="Unauthenticated"){
+            callback({
+                "status": "error",
+                "type":data.code,
+                "message":data.message
+            })
+        }else {
+            request.post(`http://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb`, {
+                headers: {
+                    "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryrKZeHVBAFJvaDv2b"
+                },
+                formData: {
+                    "key": `${projectId}/${url}`,
+                    "Content-Type": type,
+                    "Cache-Control": `max-age=31536000`,
+                    "AWSAccessKeyId": data["accessKeyId"],
+                    "acl": "public-read",
+                    "policy": data["policy"],
+                    "signature": data["signature"],
+                    "file": fs.readFileSync(file)
+                }
+            }).on('error', function (err) {
+                console.log(err.color("BgRed"));
+            })
+            .on('response', function (data) {
+                function getFilesizeInBytes(filename) {
+                    var stats = fs.statSync(filename);
+                    var fileSizeInBytes = stats.size;
+                    return Math.floor((fileSizeInBytes / (1024 * 1024)) * 10) / 10;
+                }
+                callback({
+                    "status": "uploaded",
+                    "link": `https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/${projectId}/${url}`,
+                    "filesize":Math.floor(fs.statSync(file).size/1000)
+                })
+            })
+        }
+        
+    });
+    }
+    
+}
+
+//Code by rama0dev
+
+//Code by rama0dev
+
+//Code by rama0dev
